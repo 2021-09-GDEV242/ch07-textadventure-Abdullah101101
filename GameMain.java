aa0f53b1a58cfc5780c0f52590a17c163699c7ae
Abdullah101101 <g00294851@stu.raritanval.edu>
2022-10-24 00:17:30 -0400

    Main class
    This class will be used for executing outside of blueJ

---
+++ GameMain.java
@@ -0,0 +1,19 @@
+/**
+ * The purpose for this main class is to allow
+ * the game to execute outside of BlueJ
+ * 
+ *
+ * @author Abdullah Abdulwahab
+ * @version 10.23.2022
+ */
+public class GameMain
+{
+    /**
+     * runs the game outside of blueJ
+     */
+    public static void main(String[] args)
+    {
+        Game start = new Game();
+        start.play();
+    }    
+}
